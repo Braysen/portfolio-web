9919773dc83524a20afc2f3f3fdf71d959959cdf
Braysen <btorrejoncerna@gmail.com>
2023-02-25 19:51:54 -0300

    Crea y diseña Menu Lateral

--- src/app/shared/layout/layout-master/layout-master.component.ts
+++ src/app/shared/layout/layout-master/layout-master.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MenuLateralComponent } from '../../components/menu-lateral/menu-lateral.component';
 
 @Component({
   selector: 'app-layout-master',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MenuLateralComponent],
   templateUrl: './layout-master.component.html',
   styleUrls: ['./layout-master.component.css']
 })
