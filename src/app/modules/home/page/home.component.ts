da38d3a49c7707ef522cdf95f4faf272d24344ef
Braysen <btorrejoncerna@gmail.com>
2023-03-02 14:24:31 -0300

    Agrega transiciones

--- src/app/modules/home/page/home.component.ts
+++ src/app/modules/home/page/home.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({
+        transform: 'translateX(0)',
+        opacity: 0
+      })),
+    transition('void => *', [
+      style({
+        transform: 'translateX(-100%)',
+        opacity: 0
+      }),
+      animate(800)
+    ]),
+    transition('* => void', [
+      animate(800, style({
+        transform: 'translateX(100%)',
+        opacity: 1
+      }))
+    ])
+    ])
+  ]
 })
 export class HomeComponent{
 }
